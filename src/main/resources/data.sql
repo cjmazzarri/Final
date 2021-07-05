1337780aedad4354cec86d11146acc0d33500d7e
cjmazzarri <cjmazzarri@gmail.com>
2021-07-05 12:59:27 -0500

    🗃️Added more students

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
 CREATE TABLE IF NOT EXISTS `students` (`id` INTEGER PRIMARY KEY AUTO_INCREMENT,`name` VARCHAR(250) NOT NULL, `last_name` VARCHAR(250) NOT NULL);
 
 INSERT INTO students (`id`,`name`, `last_name`) VALUES (1,'Carlos', 'Mazzarri');
+INSERT INTO students (`id`,`name`, `last_name`) VALUES (2,'Marcelo', 'Martínez');
+INSERT INTO students (`id`,`name`, `last_name`) VALUES (3,'Juan', 'Rodríguez');
+INSERT INTO students (`id`,`name`, `last_name`) VALUES (4,'Pedro', 'Pérez');
+INSERT INTO students (`id`,`name`, `last_name`) VALUES (5,'Joshua', 'Graham');
+
